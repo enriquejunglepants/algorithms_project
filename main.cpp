c40cac882cb73a7937d174e5299f7df1c9546877
enriquejunglepants <joreymelinder@my.unt.edu>
2018-03-31 00:03:41 -0500

    random graph generator

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
-    cout<<"test"<<endl;
     int size;
+    cout<<"Input number of vertices: ";
     cin>>size;
 
     for(int r=0;r<size;r++){
       for(int c=0;c<size;c++){
         int w;
-        cin>>w;
-        cout<<w;
+        //cin>>w;
+        w=rand() % 9;
+        cout<<w<<"\t";
       }
       cout<<endl;
     }
