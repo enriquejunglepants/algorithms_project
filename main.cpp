4ce7754183306aad8dbba9618d44eacfcb1f778e
enriquejunglepants <joreymelinder@my.unt.edu>
2018-03-31 00:09:44 -0500

    self-edges zero

--- main.cpp
+++ main.cpp
@@ -14,8 +14,13 @@ int main()
     for(int r=0;r<size;r++){
       for(int c=0;c<size;c++){
         int w;
-        //cin>>w;
-        w=rand() % (MAX_WEIGHT+1);
+        if(r==c){
+          w=0;
+        }
+        else{
+          w=rand() % (MAX_WEIGHT+1);
+        }
+
         cout<<w<<"\t";
       }
       cout<<endl;
